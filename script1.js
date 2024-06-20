1330f731d550221e15952153bfb98eb28f143359
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-20 19:26:11 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -16,7 +16,6 @@ function generateSignature() {
     reader.readAsDataURL(profilePic);
     reader.onloadend = function () {
         const profilePicUrl = reader.result;
-        const githubBaseUrl = 'https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/';
 
         const bolds = {
             "Noida": "normal",
@@ -32,7 +31,7 @@ function generateSignature() {
             linkedinHtml = `
             <div style="height: 15px; width: 1.5px; background-color: #999; margin: 0 2px;"></div>
             <a href="${linkedin}" target="_blank">
-                <img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px; height: 15px;">
+                <img src="data:image/png;base64,YOUR_BASE64_LINKEDIN_IMAGE" alt="LinkedIn" style="width: 15px; height: 15px;">
             </a>
             `;
         }
@@ -41,7 +40,7 @@ function generateSignature() {
         <div style="font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6; display: flex; align-items: flex-start; padding: 2px; border-radius: 5px; margin-left: 5px;">
             <div style="display: flex; flex-direction: column; align-items: center; margin-right: 10px;">
                 <img src="${profilePicUrl}" alt="Profile Picture" style="border-radius: 50%; width: 70px;">
-                <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" style="width: 60px; margin-top: 10px;">
+                <img src="data:image/png;base64,YOUR_BASE64_SCHOOL_LOGO" alt="School Logo" style="width: 60px; margin-top: 10px;">
                 <div style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 3px;">
                     <span style="font-weight: ${bolds.Noida};">Noida</span> | 
                     <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
@@ -59,24 +58,24 @@ function generateSignature() {
                 <span style="color: #a6a6a6; font-size: 12px; margin-top: 0px;">${designation}</span>
                 <div style="margin-top: 10px; display: flex; flex-direction: column; font-size: 10px;">
                     <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                        <img src="${githubBaseUrl}phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                        <img src="data:image/png;base64,YOUR_BASE64_PHONE_ICON" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="tel:${phone}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${phone}</a>
                     </div>
                     <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                        <img src="${githubBaseUrl}email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                        <img src="data:image/png;base64,YOUR_BASE64_EMAIL_ICON" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="mailto:${email}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${email}</a>
                     </div>
                     <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                        <img src="${githubBaseUrl}website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                        <img src="data:image/png;base64,YOUR_BASE64_WEBSITE_ICON" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">https://shivnadarschool.edu.in/</a>
                     </div>
                 </div>
                 <div style="margin-top: 3px; display: flex;">
-                    <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;"></a>
-                    <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;"></a>
-                    <a href="https://www.youtube.com/@shivnadarschools" style="margin-right: 10px;"><img src="${githubBaseUrl}youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;"></a>
-                    <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;"><img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px;"></a>
-                    <a href="https://www.twitter.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}twitter-icon.png?raw=true" alt="Twitter" style="width: 15px;"></a>
+                    <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="data:image/png;base64,YOUR_BASE64_FACEBOOK_ICON" alt="Facebook" style="width: 15px;"></a>
+                    <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="data:image/png;base64,YOUR_BASE64_INSTAGRAM_ICON" alt="Instagram" style="width: 15px;"></a>
+                    <a href="https://www.youtube.com/@shivnadarschools" style="margin-right: 10px;"><img src="data:image/png;base64,YOUR_BASE64_YOUTUBE_ICON" alt="YouTube" style="width: 15px;"></a>
+                    <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;"><img src="data:image/png;base64,YOUR_BASE64_LINKEDIN_ICON" alt="LinkedIn" style="width: 15px;"></a>
+                    <a href="https://www.twitter.com/shivnadarschool" style="margin-right: 10px;"><img src="data:image/png;base64,YOUR_BASE64_TWITTER_ICON" alt="Twitter" style="width: 15px;"></a>
                 </div>
             </div>
         </div>
