accbcf7f7a0ece925bb234bad478dd2fa7247185
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-21 15:37:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,56 +30,80 @@ function generateSignature() {
         let linkedinHtml = '';
         if (linkedin) {
             linkedinHtml = `
-            <div class="vertical-line-small"></div>
-            <a href="${linkedin}" target="_blank">
-                <img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" class="linkedin-logo">
-            </a>
+            <td style="vertical-align: middle; padding-left: 5px;">
+                <a href="${linkedin}" target="_blank">
+                    <img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px; height: 15px;">
+                </a>
+            </td>
             `;
         }
 
         const signatureHtml = `
-        <div class="signature-container" style="width: fit-content;">
-            <div class="left-column">
-                <img src="${profilePicUrl}" alt="Profile Picture" class="profile-pic">
-                <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" class="school-logo">
-                <div class="school-locations">
-                    <span style="font-weight: ${bolds.Noida};">Noida</span> | 
-                    <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
-                    <span style="font-weight: ${bolds.Faridabad};">Faridabad</span> | 
-                    <span style="font-weight: ${bolds.Delhi};">Delhi</span> | 
-                    <span style="font-weight: ${bolds.Chennai};">Chennai</span>
-                </div>
-            </div>
-            <div class="vertical-line"></div>
-            <div class="signature-details">
-                <div class="name-linkedin">
-                    <span class="name">${name}</span>
-                    ${linkedinHtml}
-                </div>
-                <span class="designation">${designation}</span>
-                <div class="contact-info">
-                    <div>
-                        <img src="${githubBaseUrl}phone-icon.png?raw=true" alt="Phone Icon"> 
-                        <a href="tel:${phone}">${phone}</a>
+        <table cellpadding="0" cellspacing="0" border="0" style="width: fit-content; font-family: Poppins, Arial, sans-serif; color: #a6a6a6; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; margin-left: 5px;">
+            <tr>
+                <td style="vertical-align: top; padding-right: 10px;">
+                    <img src="${profilePicUrl}" alt="Profile Picture" style="border-radius: 50%; width: 70px;">
+                    <br>
+                    <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" style="width: 60px; margin-top: 10px;">
+                    <br>
+                    <div style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 3px;">
+                        <span style="font-weight: ${bolds.Noida};">Noida</span> | 
+                        <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
+                        <span style="font-weight: ${bolds.Faridabad};">Faridabad</span> | 
+                        <span style="font-weight: ${bolds.Delhi};">Delhi</span> | 
+                        <span style="font-weight: ${bolds.Chennai};">Chennai</span>
                     </div>
-                    <div>
-                        <img src="${githubBaseUrl}email-icon.png?raw=true" alt="Email Icon"> 
-                        <a href="mailto:${email}">${email}</a>
-                    </div>
-                    <div>
-                        <img src="${githubBaseUrl}website-icon.png?raw=true" alt="Web Icon"> 
-                        <a href="https://shivnadarschool.edu.in/">https://shivnadarschool.edu.in/</a>
-                    </div>
-                </div>
-                <div class="social-icons">
-                    <a href="https://www.facebook.com/shivnadarschool"><img src="${githubBaseUrl}facebook-icon.png?raw=true" alt="Facebook"></a>
-                    <a href="https://www.instagram.com/shivnadarschool"><img src="${githubBaseUrl}instagram-icon.png?raw=true" alt="Instagram"></a>
-                    <a href="https://www.youtube.com/shivnadarschool"><img src="${githubBaseUrl}youtube-icon.png?raw=true" alt="YouTube"></a>
-                    <a href="https://www.linkedin.com/school/shiv-nadar-school/"><img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn"></a>
-                    <a href="https://www.twitter.com/shivnadarschool"><img src="${githubBaseUrl}twitter-icon.png?raw=true" alt="Twitter"></a>
-                </div>
-            </div>
-        </div>
+                </td>
+                <td style="vertical-align: top; padding: 0 10px;">
+                    <table cellpadding="0" cellspacing="0" border="0">
+                        <tr>
+                            <td style="font-weight: bold; color: #a6a6a6; font-size: 14px;">${name}</td>
+                            ${linkedinHtml}
+                        </tr>
+                        <tr>
+                            <td style="color: #a6a6a6; font-size: 12px;">${designation}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top: 10px; font-size: 10px;">
+                                <img src="${githubBaseUrl}phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                                <a href="tel:${phone}" style="color: #a6a6a6; text-decoration: none;">${phone}</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top: 2px; font-size: 10px;">
+                                <img src="${githubBaseUrl}email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                                <a href="mailto:${email}" style="color: #a6a6a6; text-decoration: none;">${email}</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top: 2px; font-size: 10px;">
+                                <img src="${githubBaseUrl}website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
+                                <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; text-decoration: none;">https://shivnadarschool.edu.in/</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top: 3px;">
+                                <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;">
+                                    <img src="${githubBaseUrl}facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;">
+                                </a>
+                                <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;">
+                                    <img src="${githubBaseUrl}instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;">
+                                </a>
+                                <a href="https://www.youtube.com/shivnadarschool" style="margin-right: 10px;">
+                                    <img src="${githubBaseUrl}youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;">
+                                </a>
+                                <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;">
+                                    <img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px;">
+                                </a>
+                                <a href="https://www.twitter.com/shivnadarschool">
+                                    <img src="${githubBaseUrl}twitter-icon.png?raw=true" alt="Twitter" style="width: 15px;">
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
         `;
         document.getElementById('signature-result').innerHTML = signatureHtml;
     };
