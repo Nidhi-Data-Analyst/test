8fe24effee41394bd70589fd2843bbb2e9dbe013
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-21 14:04:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,119 +4,105 @@ function generateSignature() {
     const phone = document.getElementById('phone').value;
     const email = document.getElementById('email').value;
     const linkedin = document.getElementById('linkedin').value;
-    const profilePicInput = document.getElementById('profilePic');
-    const profilePic = profilePicInput.files[0];
+    const profilePic = document.getElementById('profilePic').files[0];
     const campus = document.getElementById('campus').value;
 
+    if (!name || !designation || !phone || !email || !profilePic || !campus) {
+        alert('Please fill in all mandatory fields.');
+        return;
+    }
+
     const reader = new FileReader();
-    reader.onload = function(e) {
-        const profilePicBase64 = e.target.result;
+    reader.readAsDataURL(profilePic);
+    reader.onloadend = function () {
+        const profilePicUrl = reader.result;
+        const githubBaseUrl = 'https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/';
+
+        const bolds = {
+            "Noida": "normal",
+            "Gurgaon": "normal",
+            "Faridabad": "normal",
+            "Delhi": "normal",
+            "Chennai": "normal",
+        };
+        bolds[campus] = "bold";
+
+        let linkedinHtml = '';
+        if (linkedin) {
+            linkedinHtml = `
+            <div style="height: 15px; width: 1.5px; background-color: #999; margin: 0 2px;"></div>
+            <a href="${linkedin}" target="_blank">
+                <img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px; height: 15px;">
+            </a>
+            `;
+        }
 
         const signatureHtml = `
-            <div class="signature-container" style="font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6; display: flex; align-items: flex-start; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; margin-left: 5px;
-            width:100%; max-width:300px;">
-                <div class="left-column" style="display:block; margin:0px auto; text-align:center;">
-                    <img src="${profilePicBase64}" alt="Profile Picture" class="profile-pic" style="border-radius: 50%; width: 70px;">
-                    <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/school-logo.png?raw=true" alt="School Logo" class="school-logo" style="max-width: 60px; width:100%; margin-top: 10px; display:block; margin:0px auto;">
-                    <div class="school-locations" style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 3px;">
-                        <span style="font-weight: normal;">Noida</span> |
-                        <span style="font-weight: normal;">Gurgaon</span> |
-                        <span style="font-weight: normal;">Faridabad</span> |
-                        <span style="font-weight: normal;">Delhi</span> |
-                        <span style="font-weight: normal;">Chennai</span>
-                    </div>
-                </div>
-                <div class="vertical-line" style="width: 1.5px; background-color: #a6a6a6; height: 130px; margin: 0 10px;"></div>
-                <div class="signature-details" style="display:block; text-align:center;">
-                    <div class="name-linkedin" style="align-items: center;">
-                        <span class="name" style="font-weight: bold; color: #a6a6a6; font-size: 14px;">${name}</span>
-                        <div class="vertical-line-small"></div>
-                        <a href="${linkedin}" target="_blank">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-icon.png?raw=true" alt="LinkedIn" class="linkedin-logo" style="width: 15px; height: 15px;">
-                        </a>
-                    </div>
-                    <span class="designation" style="color: #a6a6a6; font-size: 12px; margin-top: 0px;">${designation}</span>
-                    <div class="contact-info" style="margin-top: 10px; display:block; clear:both; font-size: 10px;">
-                        <div style="display:block; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="tel:${phone}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${phone}</a>
-                        </div>
-                        <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="mailto:${email}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${email}</a>
-                        </div>
-                        <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">https://shivnadarschool.edu.in/</a>
-                        </div>
-                    </div>
-                    <div class="social-icons" style="margin-top: 3px; display: flex;">
-                        <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;"></a>
-                        <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;"></a>
-                        <a href="https://www.youtube.com/@shivnadarschools" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;"></a>
-                        <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px;"></a>
-                        <a href="https://www.twitter.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/twitter-icon.png?raw=true" alt="Twitter" style="width: 15px;"></a>
-                    </div>
+        <div style="display: flex; align-items: flex-start; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; width: 100%; max-width: 400px; font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6;">
+            <div style="display: flex; flex-direction: column; align-items: center; text-align: center; margin-right: 10px;">
+                <img src="${profilePicUrl}" alt="Profile Picture" style="border-radius: 50%; width: 70px;">
+                <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" style="width: 60px; margin-top: 10px;">
+                <div style="font-size: 5px; color: #0d56a2; margin-top: 3px;">
+                    <span style="font-weight: ${bolds.Noida};">Noida</span> | 
+                    <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
+                    <span style="font-weight: ${bolds.Faridabad};">Faridabad</span> | 
+                    <span style="font-weight: ${bolds.Delhi};">Delhi</span> | 
+                    <span style="font-weight: ${bolds.Chennai};">Chennai</span>
                 </div>
             </div>
+            <div style="width: 1.5px; background-color: #a6a6a6; height: 130px; margin: 0 10px;"></div>
+            <div style="display: flex; flex-direction: column; text-align: center;">
+                <div style="display: flex; align-items: center; justify-content: center;">
+                    <span style="font-weight: bold; color: #a6a6a6; font-size: 14px; margin-right: 5px;">${name}</span>
+                    ${linkedinHtml}
+                </div>
+                <span style="color: #a6a6a6; font-size: 12px; margin-top: 0px;">${designation}</span>
+                <div style="margin-top: 10px; display: flex; flex-direction: column; font-size: 10px;">
+                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                        <img src="${githubBaseUrl}phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
+                        <a href="tel:${phone}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${phone}</a>
+                    </div>
+                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                        <img src="${githubBaseUrl}email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
+                        <a href="mailto:${email}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${email}</a>
+                    </div>
+                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                        <img src="${githubBaseUrl}website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
+                        <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">https://shivnadarschool.edu.in/</a>
+                    </div>
+                </div>
+                <div style="margin-top: 3px; display: flex; justify-content: center;">
+                    <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;"></a>
+                    <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;"></a>
+                    <a href="https://www.youtube.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;"></a>
+                    <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;"><img src="${githubBaseUrl}linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px;"></a>
+                    <a href="https://www.twitter.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}twitter-icon.png?raw=true" alt="Twitter" style="width: 15px;"></a>
+                </div>
+            </div>
+        </div>
         `;
-
         document.getElementById('signature-result').innerHTML = signatureHtml;
     };
+}
 
-    if (profilePic) {
-        reader.readAsDataURL(profilePic);
-    } else {
-        // Handle case where no profile picture is uploaded
-        const signatureHtml = `
-            <div class="signature-container" style="font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6; display: flex; align-items: flex-start; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; margin-left: 5px;
-            width:100%; max-width:300px;">
-                <div class="left-column" style="display:block; margin:0px auto; text-align:center;">
-                    <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/profile-pic-placeholder.png?raw=true" alt="Profile Picture" class="profile-pic" style="border-radius: 50%; width: 70px;">
-                    <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/school-logo.png?raw=true" alt="School Logo" class="school-logo" style="max-width: 60px; width:100%; margin-top: 10px; display:block; margin:0px auto;">
-                    <div class="school-locations" style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 3px;">
-                        <span style="font-weight: normal;">Noida</span> |
-                        <span style="font-weight: normal;">Gurgaon</span> |
-                        <span style="font-weight: normal;">Faridabad</span> |
-                        <span style="font-weight: normal;">Delhi</span> |
-                        <span style="font-weight: normal;">Chennai</span>
-                    </div>
-                </div>
-                <div class="vertical-line" style="width: 1.5px; background-color: #a6a6a6; height: 130px; margin: 0 10px;"></div>
-                <div class="signature-details" style="display:block; text-align:center;">
-                    <div class="name-linkedin" style="align-items: center;">
-                        <span class="name" style="font-weight: bold; color: #a6a6a6; font-size: 14px;">${name}</span>
-                        <div class="vertical-line-small"></div>
-                        <a href="${linkedin}" target="_blank">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-icon.png?raw=true" alt="LinkedIn" class="linkedin-logo" style="width: 15px; height: 15px;">
-                        </a>
-                    </div>
-                    <span class="designation" style="color: #a6a6a6; font-size: 12px; margin-top: 0px;">${designation}</span>
-                    <div class="contact-info" style="margin-top: 10px; display:block; clear:both; font-size: 10px;">
-                        <div style="display:block; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="tel:${phone}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${phone}</a>
-                        </div>
-                        <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="mailto:${email}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${email}</a>
-                        </div>
-                        <div style="display: flex; align-items: center; margin-bottom: 2px;">
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;">
-                            <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">https://shivnadarschool.edu.in/</a>
-                        </div>
-                    </div>
-                    <div class="social-icons" style="margin-top: 3px; display: flex;">
-                        <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;"></a>
-                        <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;"></a>
-                        <a href="https://www.youtube.com/@shivnadarschools" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;"></a>
-                        <a href="https://www.linkedin.com/school/shiv-nadar-school/" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-icon.png?raw=true" alt="LinkedIn" style="width: 15px;"></a>
-                        <a href="https://www.twitter.com/shivnadarschool" style="margin-right: 10px;"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/twitter-icon.png?raw=true" alt="Twitter" style="width: 15px;"></a>
-                    </div>
-                </div>
-            </div>
-        `;
+function copyToClipboard() {
+    const signatureResult = document.getElementById('signature-result');
+    const range = document.createRange();
+    range.selectNode(signatureResult);
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges(); // to deselect
+    alert('Signature copied to clipboard. You can now paste it into your Gmail signature settings.');
+}
 
-        document.getElementById('signature-result').innerHTML = signatureHtml;
-    }
+function copyToSignatureRescue() {
+    const signatureHtml = document.getElementById('signature-result').innerHTML;
+    const tempTextarea = document.createElement('textarea');
+    tempTextarea.value = signatureHtml;
+    document.body.appendChild(tempTextarea);
+    tempTextarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextarea);
+    alert('HTML copied to clipboard for Signature Rescue.');
 }
