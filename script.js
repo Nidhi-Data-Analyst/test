0472a9448e30016c92e5f4c57432f91084afbd4f
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-21 13:28:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ function generateSignature() {
         }
 
         const signatureHtml = `
-        <div class="signature-container" style="font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6; display: flex; align-items: flex-start; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; margin-left: 5px; 
+        <div class="signature-container" style="font-family: Poppins, Arial, sans-serif; line-height: 1.5; color: #a6a6a6; display: Block; align-items: flex-start; border: 1px solid #e0e0e0; padding: 2px; border-radius: 5px; margin-left: 5px; 
             width: 100px;">
-            <div class="left-column" style="display: flex; flex-direction: column; align-items: center; margin-right: 10px;">
+            <div class="left-column" style="display: block; flex-direction: column; align-items: center; margin-right: 5px;">
                 <img src="${profilePicUrl}" alt="Profile Picture" class="profile-pic" style="border-radius: 50%; width: 70px;">
                 <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" class="school-logo" style="width: 60px; margin-top: 10px;">
                 <div class="school-locations" style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 3px;">
@@ -52,27 +52,27 @@ function generateSignature() {
                 </div>
             </div>
             <div class="vertical-line" style="width: 1.5px; background-color: #a6a6a6; height: 130px; margin: 0 10px;"></div>
-            <div class="signature-details" style="display: flex; flex-direction: column;">
-                <div class="name-linkedin" style="display: flex; align-items: center;">
+            <div class="signature-details" style="display: Block; flex-direction: column;">
+                <div class="name-linkedin" style="display: Block; align-items: center;">
                     <span class="name" style="font-weight: bold; color: #a6a6a6; font-size: 14px; margin-right: 5px; margin-bottom: 1px;">${name}</span>
                     ${linkedinHtml}
                 </div>
                 <span class="designation" style="color: #a6a6a6; font-size: 12px; margin-top: 0px;">${designation}</span>
-                <div class="contact-info" style="margin-top: 10px; display: flex; flex-direction: column; font-size: 10px;">
-                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                <div class="contact-info" style="margin-top: 10px; display: Block; flex-direction: column; font-size: 10px;">
+                    <div style="display: Block; align-items: center; margin-bottom: 2px;">
                         <img src="${githubBaseUrl}phone-icon.png?raw=true" alt="Phone Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="tel:${phone}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${phone}</a>
                     </div>
-                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                    <div style="display: Block; align-items: center; margin-bottom: 2px;">
                         <img src="${githubBaseUrl}email-icon.png?raw=true" alt="Email Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="mailto:${email}" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">${email}</a>
                     </div>
-                    <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                    <div style="display: Block; align-items: center; margin-bottom: 2px;">
                         <img src="${githubBaseUrl}website-icon.png?raw=true" alt="Web Icon" style="width: 16px; vertical-align: middle; margin-right: 5px;"> 
                         <a href="https://shivnadarschool.edu.in/" style="color: #a6a6a6; font-size: 10px; margin-left: 5px; text-decoration: none;">https://shivnadarschool.edu.in/</a>
                     </div>
                 </div>
-                <div class="social-icons" style="margin-top: 3px; display: flex;">
+                <div class="social-icons" style="margin-top: 3px; display: Block;">
                     <a href="https://www.facebook.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}facebook-icon.png?raw=true" alt="Facebook" style="width: 15px;"></a>
                     <a href="https://www.instagram.com/shivnadarschool" style="margin-right: 10px;"><img src="${githubBaseUrl}instagram-icon.png?raw=true" alt="Instagram" style="width: 15px;"></a>
                     <a href="https://www.youtube.com/@shivnadarschools" style="margin-right: 10px;"><img src="${githubBaseUrl}youtube-icon.png?raw=true" alt="YouTube" style="width: 15px;"></a>
