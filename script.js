f03c9ad0113fba8acdf2188758c5c2c3679366af
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-24 10:27:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ function generateSignature() {
             <table>
                 <tr>
                     <td style="vertical-align: top; padding-right: 10px;">
-                        <img src="${profilePicUrl}" alt="Profile Picture" class="profile-pic" style="border-radius: 50%; max-width:70px; width:100%; margin-left: auto; margin-rigth: auto;">
-                        <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" class="school-logo" style=style="width:100%; max-width:60px; display:block; margin-top: 3px; margin-left: auto; margin-rigth: auto;">
-                        <div class="school-locations" style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 5px; width: 100%; clear: both; max-width: 60px;">
+                        <img src="${profilePicUrl}" alt="Profile Picture" class="profile-pic" style="border-radius: 50%; max-width:80px; width:100%; margin-left: auto; margin-rigth: auto;">
+                        <img src="${githubBaseUrl}school-logo.png?raw=true" alt="School Logo" class="school-logo" style=style="width:100%; max-width:70px; display:block; margin-top: 3px; margin-left: auto; margin-rigth: auto;">
+                        <div class="school-locations" style="font-size: 5px; color: #0d56a2; text-align: center; margin-top: 5px; clear: both;">
             width:100%;>
                             <span style="font-weight: ${bolds.Noida};">Noida</span> | 
                             <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
