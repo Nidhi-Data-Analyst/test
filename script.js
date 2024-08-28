9f28db3d4d36e681c60cb0f38366dd956cae4f40
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-08-28 14:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function generateSignature() {
                     <td style="vertical-align: top; padding-right: 3px; text-align: center; width: 125px">
                         <img src="${profilePicUrl}" alt="Profile Picture" style="border-radius: 50%; max-width:80px; width:100%; text-align:center;">
                         <br>
-                        <img src="${githubBaseUrl}school_logo14.png?raw=true" alt="School Logo" class="school-logo" style="width:100%; max-width:100px; display: block; margin: 3px auto 0;">
+                        <img src="${githubBaseUrl}Redsign.png?raw=true" alt="School Logo" class="school-logo" style="width:100%; max-width:100px; display: block; margin: 3px auto 0;">
                         <div class="school-locations" style="font-size: 6px; color: #0d56a2; text-align: center; margin-top: 3px;">
                             <span style="font-weight: ${bolds.Noida};">Noida</span> | 
                             <span style="font-weight: ${bolds.Gurgaon};">Gurgaon</span> | 
